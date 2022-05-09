e5b9ceb4119f824175f92c0b78e8b1a9f4ead327
DANIEL ASAKPA <93812021+danielasakpa@users.noreply.github.com>
2022-05-09 18:44:37 +0300

    add active color when scrolling to nav link

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-// function reveal() {
-//     var reveals = document.querySelectorAll(".reveal");
-  
-//     for (var i = 0; i < reveals.length; i++) {
-//       var windowHeight = window.innerHeight;
-//       var elementTop = reveals[i].getBoundingClientRect().top;
-//       var elementVisible = 150;
-  
-//       if (elementTop < windowHeight - elementVisible) {
-//         reveals[i].classList.add("active");
-//       } else {
-//         reveals[i].classList.remove("active");
-//       }
-//     }
-//   }
-  
-//   window.addEventListener("scroll", reveal);
+// Get all sections that have an ID defined
+const sections = document.querySelectorAll("section[id]");
 
-const sections = document.querySelectorAll("section");
-const navLi = document.querySelectorAll("nav label nav-1 li a");
-window.onscroll = () => {
-  var current = "";
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
 
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
-    if (pageYOffset >= sectionTop - 60) {
-      current = section.getAttribute("id"); }
-  });
-
-  navLi.forEach((a) => {
-    a.classList.remove.remove("active");
-    if (a.classList.contains(current)) {
-      a.classList.add("active");
+function navHighlighter() {
+  
+  // Get current scroll position
+  let scrollY = window.pageYOffset;
+  
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+    
+    /*
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+    */
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector(".navbar-1 a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(".navbar-1 a[href*=" + sectionId + "]").classList.remove("active");
     }
   });
-};
+}
