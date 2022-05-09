12f38d0e7198b1b7ab7a769f0b0d24b03e6f9770
DANIEL ASAKPA <93812021+danielasakpa@users.noreply.github.com>
2022-05-09 18:15:41 +0300

    add active selector to nav link

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
+// function reveal() {
+//     var reveals = document.querySelectorAll(".reveal");
   
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
+//     for (var i = 0; i < reveals.length; i++) {
+//       var windowHeight = window.innerHeight;
+//       var elementTop = reveals[i].getBoundingClientRect().top;
+//       var elementVisible = 150;
   
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+//       if (elementTop < windowHeight - elementVisible) {
+//         reveals[i].classList.add("active");
+//       } else {
+//         reveals[i].classList.remove("active");
+//       }
+//     }
+//   }
+  
+//   window.addEventListener("scroll", reveal);
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav nav-1 li");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
     }
-  }
-  
-  window.addEventListener("scroll", reveal);
-  
+  });
+};
